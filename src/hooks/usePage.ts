bb21d3aebeb24ee5df48823d1c5839bbb54fd86c
ZXDsuv <1650490521@qq.com>
2024-06-04 11:14:52 +0800

    feat：创建项目

---
+++ src/hooks/usePage.ts
@@ -0,0 +1,64 @@
+import { ref } from "vue";
+import type PageSearch from "@/components/PageSearch/index.vue";
+import type PageContent from "@/components/PageContent/index.vue";
+import type PageModal from "@/components/PageModal/index.vue";
+import type { IOperatData, IObject } from "@/components/PageContent/index.vue";
+export type { IOperatData, IObject };
+
+function usePage() {
+  const searchRef = ref<InstanceType<typeof PageSearch>>();
+  const contentRef = ref<InstanceType<typeof PageContent>>();
+  const addModalRef = ref<InstanceType<typeof PageModal>>();
+  const editModalRef = ref<InstanceType<typeof PageModal>>();
+
+  // 搜索
+  function handleQueryClick(queryParams: IObject) {
+    contentRef.value?.fetchPageData(queryParams, true);
+  }
+  // 重置
+  function handleResetClick(queryParams: IObject) {
+    contentRef.value?.fetchPageData(queryParams, true);
+  }
+  // 新增
+  function handleAddClick() {
+    //显示添加表单
+    addModalRef.value?.setModalVisible();
+  }
+  // 编辑
+  function handleEditClick(row: IObject) {
+    //显示编辑表单 根据数据进行填充
+    editModalRef.value?.setModalVisible(row);
+  }
+  // 表单提交
+  function handleSubmitClick() {
+    //根据检索条件刷新列表数据
+    const queryParams = searchRef.value?.getQueryParams();
+    contentRef.value?.fetchPageData(queryParams, true);
+  }
+  // 导出
+  function handleExportClick() {
+    // 根据检索条件导出数据
+    const queryParams = searchRef.value?.getQueryParams();
+    contentRef.value?.exportPageData(queryParams);
+  }
+  // 搜索显隐
+  function handleSearchClick() {
+    searchRef.value?.toggleVisible();
+  }
+
+  return {
+    searchRef,
+    contentRef,
+    addModalRef,
+    editModalRef,
+    handleQueryClick,
+    handleResetClick,
+    handleAddClick,
+    handleEditClick,
+    handleSubmitClick,
+    handleExportClick,
+    handleSearchClick,
+  };
+}
+
+export default usePage;
